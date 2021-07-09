d217867a17704e4e775e86976e222394cc2e54f0
Tanzina Rahman Mona <77913975+TanzinaRahman@users.noreply.github.com>
2021-07-10 01:43:33 +0600

    Create PythonProgram19.py

---
+++ PythonProgram19.py
@@ -0,0 +1,20 @@
+'''
+Else if = Elif
+statement 1  -> if statement
+statements 2 -> if statement , else statement
+statements 3 or more  -> if statement , elif statement....., else statement
+'''
+
+marks = 80
+if marks >= 80 :
+    print("A+")
+elif marks >= 70:
+        print("A")
+elif marks >= 60:
+            print("A-")
+elif marks >= 50:
+                print("D")
+
+           
+
+
